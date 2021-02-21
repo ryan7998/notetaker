40d1e2c2960139486e8514777aa27ccb58bab912
fazle <ryan7998@gmail.com>
2021-02-20 23:19:13 -0500

    writefile to json updated

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+// used uuid for unique ids
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const path = require('path');
 const notes = require('./db/db');
 const express = require('express');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Middleware: serve static assets:
@@ -15,57 +16,56 @@ app.use(express.json());
 
 
 // Functions:
-function createNewNotes(body, notesArray){
 
-    const note = body;
-    notesArray.push(note);
+// function to update file into the json file:
+function writeFile(notesArray){
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
         JSON.stringify(notesArray, null, 2)
     );
+}
+
+// function to create new notes:
+function createNewNotes(body, notesArray){
+
+    const note = body;
+    notesArray.push(note);
+    writeFile(notesArray);
     return note;
 }
 
 function deleteById(id, notesArray){
-    // delete notesArray[0];
 
    for(let i=0; i<notesArray.length; i++){
        if(notesArray[i].id == id){
            notesArray.splice(i, 1);
        }
    }
-    /* notesArray.splice(0,1);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-    ); */
-    return id;
+   return notesArray;
 }
 
 // routing apis:
 app.get('/api/notes', (req, res)=>{
     let results = notes;
-    // console.log(results);
     res.json(results);
 });
 
 // routing post api:
 app.post('/api/notes', (req, res)=>{
     req.body.id = uuidv4();
-    // console.log(req.body);
-    // if any data in req.body is incorrect:
     const note = createNewNotes(req.body, notes);
     res.json(note);
-    //console.log(note);
 });
 
 // routing delete api:
 app.delete('/api/notes/:id', (req, res)=>{
-    const deleteNote = deleteById(req.params.id, notes);
-    res.json(deleteNote);
+    const newNotesArray = deleteById(req.params.id, notes);
+    writeFile(newNotesArray);
+    res.json(newNotesArray);
 })
 
 
+// ROUTING
 // routing html:
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -80,5 +80,5 @@ app.get('/', (req, res)=>{
 
 // listening in port 3002
 app.listen(PORT, ()=>{
-    console.log('API server now in port 3002');
+    console.log(`API server now in port ${PORT}`);
 });
